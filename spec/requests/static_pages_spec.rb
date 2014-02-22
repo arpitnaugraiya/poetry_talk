b722db51a174beacdec18bec0d93c9c8a35c645e
arpit <sonu.be07@gmail.com>
2014-02-22 16:16:28 +0530

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       expect(page).to have_content('Poetry Talk')
     end
     
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit'/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Poetry Talk | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Poetry Talk")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(' | Home')
     end
   end
   
